54d443e3f0b71e50005cbc35b1f8e7bf1f24187c
azure-sea <205392123@qq.com>
2020-11-03 16:56:31 +0800

    公共库代码构建

--- src/org/devops/public_build.groovy
+++ src/org/devops/public_build.groovy
@@ -1,10 +1,6 @@
 package org.devops
 
 // 更新公共库并构建
-def public_build(gitpublicuser,srcUrl,branchName){
-	checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], 
-				  doGenerateSubmoduleConfigurations: false, 
-				  extensions: [], 
-				  submoduleCfg: [], 
-				  userRemoteConfigs: [[credentialsId: '${gitpublicuser}', url: "${srcUrl}"]]])
+def public_build(){
+	sh "/usr/bin/python3 setup.py bdist_wheel --universal"
 }
