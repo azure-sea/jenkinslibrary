0a5b51b83a66343c29e2e0a9c90d43a732fad2b0
azure-sea <205392123@qq.com>
2020-11-05 18:10:17 +0800

    澧炲姞checkoutSCM妯″潡

--- src/org/devops/public_build.groovy
+++ src/org/devops/public_build.groovy
@@ -1,8 +1,22 @@
 package org.devops
 
 // 更新公共库并构建
-def public_build(){
-    sh "pwd"
-	sh "/usr/bin/pip3 install wheel"
-	sh "/usr/bin/python3 setup.py bdist_wheel --universal"
+def public_build(git_user_auth,public_branch_or_tag,git_url){
+	checkout([$class: 'GitSCM', branches: [[name: "${public_branch_or_tag}"]], 
+			doGenerateSubmoduleConfigurations: false, 
+			extensions: [], 
+			submoduleCfg: [], 
+			userRemoteConfigs: [[credentialsId: '${git_user_auth}', 
+			url: "${git_url}"]]])
+}
+
+def checkoutSCM(branch,repoURL,credential){
+	checkout changlog: false,
+	                poll: false,
+					scm: [$class: 'GitSCM',],
+						branches: [[name: branch]],
+							extensions: [[$class: 'RelativeTargetDirectory',
+							relativeTargetDir: repoURL.split('[/\\.]+').[-2].trim()],
+									[$class: 'CleanBeforeCheckout']]],
+							userRemoteConfigs: [[credentialsId: credential, url: repoURL]]]		
 }
