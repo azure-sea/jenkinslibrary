dd29cb642e5f70ef0d2fb8ac7c9e70801f021723
azure-sea <2053921233@qq.com>
2020-12-02 11:53:54 +0800

    提示需要的插件

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,6 @@
 package org.devops
 
-// 打印内容
+// 打印内容 依赖插件 "ansiColor"
 def PrintMes(value,color){
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m ${value} \033[0m",
