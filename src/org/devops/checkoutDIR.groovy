61af426ecaa758bf17d97a261ea4a7ea670f9b93
azure-sea <2053921233@qq.com>
2021-02-04 13:53:05 +0800

    只更新目录

---
+++ src/org/devops/checkoutDIR.groovy
@@ -0,0 +1,14 @@
+package org.devops
+
+// git 拉取 保留项目目录。例如 jenkins.git 拉取后在当前工作目录下存在 jenkins 目录 
+def checkoutSCMDIR(branch,gitcheckpath,repoURL,credential){
+	checkout changlog: false,
+	                poll: false,
+					scm: [$class: 'GitSCM',
+						branches: [[name: branch]],
+							extensions: [[$class: 'RelativeTargetDirectory',
+							relativeTargetDir: repoURL.split('[/\\.]+')[-2].trim()],
+							[$class: 'CleanBeforeCheckout']
+							[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'gitcheckpath']]],
+							userRemoteConfigs: [[credentialsId: credential, url: repoURL]]]		
+}
